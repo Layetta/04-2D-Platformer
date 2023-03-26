83cb020f65b2103ea63fa36230802b942a13ecc9
Layetta <aplawill@iu.edu>
2023-03-26 19:44:44 -0400

    Continued progress

--- Camera.gd
+++ Camera.gd
@@ -1,6 +1,14 @@
 extends Camera2D
 
+onready var coins = get_node_or_null("/root/Game/Coins")
+
 func _process(_delta):
 	var player = get_node_or_null("/root/Game/Player_Container/Player")
 	if player != null:
 		position = player.position
+		var score_text = get_node_or_null("/root/Game/Score")
+		score_text.set_position(position+Vector2(-20,-60))
+
+
+		score_text.set_text("Score: "+String(coins.numCoinsFound))
+		
--- Global.gd
+++ Global.gd
@@ -1,5 +1,10 @@
 extends Node
 
+func _ready():
+	OS.window_position = Vector2(0,0)
+	OS.window_size = Vector2(1080,800)
+	
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
